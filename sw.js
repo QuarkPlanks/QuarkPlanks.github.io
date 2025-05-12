6e362665c9a725a4de6e8feddc1310e71c5e772d
QuarkPlanks <1412580863@qq.com>
2025-05-12 11:36:33 +0800

    Site updated: 2025-05-12 11:36:32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1747020709866";const preCache = ["/images/taichi.png","/images/banner3.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1747020987879";const preCache = ["/images/taichi.png","/images/banner3.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
